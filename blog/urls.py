41407c8c7e03880f3e9b539d863c71758afe7ba3
Cameron Campbell <cameron.campbell918@yahoo.com>
2021-10-26 20:41:53 -0700

    Competency Report

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, HomePageView
+from .views import (
+    PostListView,
+    PostDetailView,
+    HomePageView,
+    PostCreateView,
+    PostUpdateView
+)
 
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('posts/', PostListView.as_view(), name='all_posts'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/', PostListView.as_view(), name='all_posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostCreateView.as_view(), name="post_new"),
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name="post_edit"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, TemplateView
+from django.views.generic import ListView, DetailView, TemplateView, UpdateView
+from django.views.generic.edit import CreateView
 from .models import Post
 
 # Create your views here.
 
+
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+
 class PostListView(ListView):
     model = Post
     template_name = "post_list.html"
+
 
 class PostDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
 
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "body", "author"]
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
